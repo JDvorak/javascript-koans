13f6138194ecc53f1547cb84294f8d1acd60161c
Hack Reactor Students <accounts@hackreactor.com>
2013-03-09 10:56:59 -0800

    Fixed Broken Test

--- koans/AboutFunctions.js
+++ koans/AboutFunctions.js
@@ -104,11 +104,10 @@ describe("About Functions", function() {
       return a * b;
     };
     expect(multiply.toString()).toBe(
-      "function (a, b) {
-        //An internal comment
-        return a * b;
-      }"
-    );
+      "function (a, b) {\n\
+      //An internal comment\n\
+      return a * b;\n\
+    }");
   });
 
 
